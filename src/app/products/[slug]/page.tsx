80d8e1701e729bf7425d07ba634accb921e6badf
Ismail Ahmed <aismail@kictl.com>
2025-01-26 23:00:25 +0500

    dynamic routes added

---
+++ src/app/products/[slug]/page.tsx
@@ -0,0 +1,57 @@
+import { client } from "@/sanity/lib/client";
+import { Product } from "../../../types/products";
+import { groq } from "next-sanity";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
+
+
+interface Props {
+    params : Promise<{slug: string}>;
+}
+
+async function fetchProduct(slug: string): Promise<Product> {
+    return client.fetch(  groq `*[_type == "product" && slug.current == $slug][0]{
+        _id,
+        name,
+        _type,
+        price,
+        image,
+        description,
+        slug
+        }`, {slug});
+    }
+    
+    export default async function ProductPage({params}: Props) {
+        const { slug } = await params;
+        const product = await fetchProduct(slug);
+       return (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <div className="aspect-square w-full h-96 md:h-auto md:w-full">
+                    {product.image && (
+                        <Image
+                            src={urlFor(product.image).url()}
+                            alt={product.name}
+                            width={300}
+                            height={300}
+                            className="w-full h-full object-cover rounded-md shadow-md"
+                        />
+                    )}
+                </div>
+                <div className="flex flex-col justify-center gap-8">
+                    <h1 className="text-3xl font-semibold">
+                        {product.name}
+                    </h1>
+                    <p className="text-lg font-sans">
+                        {product.price ? `$${product.price}` : "Price not available"}
+                    </p>
+                    <p className="text-lg font-sans">
+                        {product.description}
+                    </p>
+                </div>
+
+            </div>
+
+        </div>
+        );
+}
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image';
 import { FiHeart, FiShoppingCart } from "react-icons/fi";
 import { FaAngleDown } from 'react-icons/fa6';
@@ -7,105 +9,119 @@ import { TiThList } from "react-icons/ti";
 import { FaCircle } from "react-icons/fa";
 
 import Link from 'next/link';
+import { Product } from '../../types/products';
+import { client } from '@/sanity/lib/client';
+import { allproducts } from '@/sanity/lib/quries';
+import { urlFor } from '@/sanity/lib/image';
 function Products() {
-    const featuredProducts = [
-        {
-          id: 15,
-          name: "Vel elit euismod",
-          discountPrice: 25.00,
-          price: 42.00,
-          image: "/product-15.png",
-          height: 201,
-          width: 201
-        },
-        {
-          id: 16,
-          name: "Ultricies condimentum imperdiet",
-          discountPrice: 25.00,
-          price: 42.00,
-          image: "/product-16.png",
-          height: 169,
-          width: 169
-        },
-        {
-          id: 17,
-          name: "Vitae suspendisse sed",
-          discountPrice: 25.00,
-          price: 42.00,
-          image: "/product-17.png",
-          height: 201,
-          width: 201
-        },
-        {
-          id: 18,
-          name: "Sed at fermentum",
-          discountPrice: 25.00,
-          price: 42.00,
-          image: "/product-18.png",
-          height: 188,
-          width: 188
-        },
-        {
-            id: 19,
-            name: "Fusce pellentesque at",
-            price: 42.0,
-            discountPrice: 25.00,
-            image: "/product-19.png",
-            height: 139,
-            width: 128 
-          },
-          {
-            id: 20,
-            name: "Vestibulum magna laoreet",
-            price: 42.00,
-            discountPrice: 25.00,
-            image: "/product-20.png",
-            height: 158,
-            width: 154 
-          },
-          {
-            id: 21,
-            name: "Sollicitudin amet orci",
-            price: 42.0,
-            discountPrice: 25.00,
-            image: "/product-21.png",
-            height: 144,
-            width: 114 
-          },
-          {
-            id: 22,
-            name: "Ultrices mauris sit",
-            price: 42.00,
-            discountPrice: 25.00,
-            image: "/product-22.png",
-            height: 167,
-            width: 167 
-          },
-          {
-            id: 23,
-            name: "Pellentesque condimentum ac",
-            price: 42.00,
-            discountPrice: 25.00,
-            image: "/product-23.png",
-            height: 151,
-            width: 170 
-          },
+    // const featuredProducts = [
+    //     {
+    //       id: 15,
+    //       name: "Vel elit euismod",
+    //       discountPrice: 25.00,
+    //       price: 42.00,
+    //       image: "/product-15.png",
+    //       height: 201,
+    //       width: 201
+    //     },
+    //     {
+    //       id: 16,
+    //       name: "Ultricies condimentum imperdiet",
+    //       discountPrice: 25.00,
+    //       price: 42.00,
+    //       image: "/product-16.png",
+    //       height: 169,
+    //       width: 169
+    //     },
+    //     {
+    //       id: 17,
+    //       name: "Vitae suspendisse sed",
+    //       discountPrice: 25.00,
+    //       price: 42.00,
+    //       image: "/product-17.png",
+    //       height: 201,
+    //       width: 201
+    //     },
+    //     {
+    //       id: 18,
+    //       name: "Sed at fermentum",
+    //       discountPrice: 25.00,
+    //       price: 42.00,
+    //       image: "/product-18.png",
+    //       height: 188,
+    //       width: 188
+    //     },
+    //     {
+    //         id: 19,
+    //         name: "Fusce pellentesque at",
+    //         price: 42.0,
+    //         discountPrice: 25.00,
+    //         image: "/product-19.png",
+    //         height: 139,
+    //         width: 128 
+    //       },
+    //       {
+    //         id: 20,
+    //         name: "Vestibulum magna laoreet",
+    //         price: 42.00,
+    //         discountPrice: 25.00,
+    //         image: "/product-20.png",
+    //         height: 158,
+    //         width: 154 
+    //       },
+    //       {
+    //         id: 21,
+    //         name: "Sollicitudin amet orci",
+    //         price: 42.0,
+    //         discountPrice: 25.00,
+    //         image: "/product-21.png",
+    //         height: 144,
+    //         width: 114 
+    //       },
+    //       {
+    //         id: 22,
+    //         name: "Ultrices mauris sit",
+    //         price: 42.00,
+    //         discountPrice: 25.00,
+    //         image: "/product-22.png",
+    //         height: 167,
+    //         width: 167 
+    //       },
+    //       {
+    //         id: 23,
+    //         name: "Pellentesque condimentum ac",
+    //         price: 42.00,
+    //         discountPrice: 25.00,
+    //         image: "/product-23.png",
+    //         height: 151,
+    //         width: 170 
+    //       },
 
-      ];
+    //   ];
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+      async function fetchProducts() {
+        const fetchProducts : Product[] = await client.fetch(allproducts);
+        setProducts(fetchProducts);
+        
+      }
+      fetchProducts();
+    }, []);
     
 
   return (
     <div>
-      <main className='bg-maingray w-full h-[266px] flex items-center'>
+      <main className='bg-blue-50 w-full h-[266px] flex items-center'>
         <div className="mx-auto lg:mx-[200px] 2xl:mx-default-margin ">
             <h1 className="text-headingsText text-[36px] leading-[42.19px] text-center lg:text-left">Shop Grid Default</h1>
-            <p className= "font-medium text-[16px] leading-[19.2px] text-center lg:text-left text-black">Home. Phones. <span className='text-secondary'>Shop Grid Default</span></p>
+            <p className= "font-medium text-[16px] leading-[19.2px] text-center lg:text-left text-black">Home. Phones. <span className='text-red-400'>Shop Grid Default</span></p>
         </div>
       </main>
 
       <div className="my-16 lg:my-32  mx-auto lg:mx-[200px] 2xl:mx-default-margin block space-y-3 lg:flex lg:items-center lg:justify-between">
         <div className="">
-          <h1 className='text-headingsText text-2xl text-center md:text-left md:text-[22px] md:leading-[25.78px] font-semibold'>Ecommerce Acceories & Fashion item </h1>
+          <h1 className='text-headingsText text-2xl text-center md:text-left md:text-[22px] md:leading-[25.78px] font-semibold'>Ecommerce Accessories & Fashion items </h1>
         <p className='font-normal text-[12px] leading-[14.4px] text-[#8A8FB9] text-center md:text-left'>About 9,620 results (0.62 seconds)</p>
         </div>
         <div className=" block md:flex items-center justify-between gap-2 mx-4 md:mx-0 ">
@@ -134,11 +150,35 @@ function Products() {
 
 
      
-      <section className="lg:mb-14">
+      <section className="max-w-6xl mx-auto px-4 py-8">
       <div className=" lg:mx-[200px] 2xl:mx-default-margin mx-auto">
-
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-center ">
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+          {products.map((product) => (
+            <div key={product._id} 
+            className='border rounded-lg shadow-md p-4 hover:shadow-lg transition duration-200'>
+              <Link href={`/products/${product.slug.current}`}>
+              {product.image && (
+                <Image
+                src={urlFor(product.image).url()}
+                alt={product.name}
+                width={300}
+                height={300}
+                className='w-full h-48 object-cover rounded-md'
+                />
+              )}
+              <h2 className='text-lg font-semibold mt-4'>{product.name}</h2>
+              <p>
+                {product.price ? `$${product.price}` : "Price not available"}
+                {product.name}
+                {""}
+                {product.description}
+              </p>
+              </Link>
+              </div>
+            ))}
+          </div>
+              
+        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-center ">
           {featuredProducts.map((product) => (
             <div
               key={product.id}
@@ -164,7 +204,7 @@ function Products() {
 
 
               <div className="p-4 md:text-center">
-               <Link href={`/products/${product.id}`}>
+               <Link href={/products/${product.id}}>
                <h3 className="text-xl font-bold text-headingsText">
                   {product.name}
                 </h3>
@@ -179,7 +219,7 @@ function Products() {
               </div>
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
       
     </section>
